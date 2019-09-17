5021936fe113c4b20b2eebee9fee6bb891410771
Lukas Gentele <lg@devspace.cloud>
2019-09-16 21:08:01 -0700

    fix index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo `
+echo '
 <html>
     <head>
         <link rel="stylesheet" href="https://devspace.cloud/quickstart.css">
@@ -9,7 +9,7 @@ echo `
         <img src="https://devspace.cloud/img/congrats.gif" />
         <h1>You deployed this project with DevSpace!</h1>
         <div>
-            <h2>Now it's time to start the development mode:</h2>
+            <h2>Now it\'s time to start the development mode:</h2>
             <ol>
                 <li>Press CTRL+C or ENTER to terminate <code>devspace open</code></li>
                 <li>Run: <code>devspace dev</code></li>
@@ -19,4 +19,4 @@ echo `
         </div>
     </body>
 </html>
-`;
+';
