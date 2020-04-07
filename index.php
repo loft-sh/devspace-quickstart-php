d2b9685eb7ea51d769a28683ed56b1141fbe3448
Lukas Gentele <lg@devspace.cloud>
2020-04-07 05:36:23 +0200

    improve quickstart

--- index.php
+++ index.php
@@ -9,11 +9,10 @@ echo '
         <img src="https://devspace.cloud/img/congrats.gif" />
         <h1>You deployed this project with DevSpace!</h1>
         <div>
-            <h2>Now it\'s time to start the development mode:</h2>
+            <h2>Now it\'s time to code:</h2>
             <ol>
-                <li>Press CTRL+C or ENTER to terminate <code>devspace open</code></li>
-                <li>Run: <code>devspace dev</code></li>
                 <li>Edit this text in <code>index.php</code> and save the file</li>
+                <li>Check the logs to see how DevSpace restarts your container</li>
                 <li>Reload browser to see the changes</li>
             </ol>
         </div>
